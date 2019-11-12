433d9bc11b7415f5844484587f05966c4be82690
495929699 <495929699g@gmail.com>
2019-11-12 23:56:25 +0800

    文件精简

--- SwiftListKit/Classes/ListAdapter+UICollectionView.swift
+++ SwiftListKit/Classes/ListAdapter+UICollectionView.swift
@@ -7,5 +7,104 @@
 
 import UIKit
 
+final class ListDataSource: NSObject, UICollectionViewDataSource {
+    private var sectionMap: SectionMap
+    
+    init(sectionMap: SectionMap) {
+        self.sectionMap = sectionMap
+        super.init()
+    }
+    
+    func update(section map: SectionMap) {
+        self.sectionMap = map;
+    }
+    
+    
+    // MARK: - UICollectionViewDataSource
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return sectionMap.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard sectionMap.count != 0 else {
+            return 0
+        }
+        return sectionMap[section].numberOfItems()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return sectionMap[indexPath.section]
+            .cellForItem(at: indexPath.row)
+    }
+    
+}
 
 
+
+final class ListDelegate: NSObject,
+UICollectionViewDelegate,
+UICollectionViewDelegateFlowLayout {
+    
+    private var sectionMap: SectionMap
+    
+    init(sectionMap: SectionMap) {
+        self.sectionMap = sectionMap
+        super.init()
+    }
+    
+    func update(section map: SectionMap) {
+        self.sectionMap = map;
+    }
+    
+    // MARK: - UICollectionViewDelegate
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+      //        collectionDelegate 代理事件出去
+        sectionMap[indexPath.section]
+            .didSelectItem(at: indexPath.item)
+    }
+      
+    func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+          
+    }
+      
+    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+          
+    }
+      
+    func collectionView(_ collectionView: UICollectionView, didEndDisplaying cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+          
+    }
+      
+      // MARK: - UICollectionViewDelegateFlowLayout
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = sectionMap[indexPath.section]
+            .sizeForItem(at: indexPath.row)
+        let positiveSize = CGSize(
+            width: max(size.width, 0),
+            height: max(size.height, 0)
+        )
+        return positiveSize
+    }
+          
+      public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+          return sectionMap[section].inset
+      }
+      
+      public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+          return sectionMap[section].minimumLineSpacing
+      }
+      
+      public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+          return sectionMap[section].minimumInteritemSpacing
+      }
+    
+    //    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+    //
+    //    }
+    //
+    //    public func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+    //
+    //    }
+    
+}
---
+++ SwiftListKit/Classes/ListAdapterType.swift
@@ -0,0 +1,21 @@
+//
+//  ListAdapterDataSource.swift
+//  SwiftListKit
+//
+//  Created by rongheng on 2019/11/5.
+//
+
+import Foundation
+
+// MARK: - ListAdapterDataSource
+public protocol ListAdapterDataSource: class {
+    func objects<Item: SectionItem>(for listAdapter: ListAdapter) -> [Item]
+    func listAdapter<Item: SectionItem>(_ listAdapter: ListAdapter, sectionControllerFor object: Item) -> ListSectionController
+    func emptyView(for listAdapter: ListAdapter) -> UIView?
+}
+
+// MARK: - ListAdapterDelegate
+public protocol ListAdapterDelegate: class {
+    func listAdapter<Item: SectionItem>(_ listAdapter: ListAdapter, willDisplay object: Item, at index: Int)
+    func listAdapter<Item: SectionItem>(_ listAdapter: ListAdapter, didEndDisplay object: Item, at index: Int)
+}
